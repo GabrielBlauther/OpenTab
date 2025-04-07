c3447ac4c84eb2def13da7f00581969fae52e32d
Gabriel Blauther <gblauther@gmail.com>
2025-04-07 18:02:58 +0000

    adiciona scripts `lint:check` e `lint:fiz`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # OpenTab
+
 Implementação do https://www.tabnews.com.br/ para criação de um Projeto/Estudo
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Home(){
-    return <h1>Hello linda, amo você!!</h1>
+function Home() {
+  return <h1>Hello linda, amo você!!</h1>;
 }
 
+function teste() {
+  console.log("indentação errada");
+}
 export default Home;
